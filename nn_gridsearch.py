37c1013a27145a41f54ebce18d5f49d4ae32bfce
Paul <paul.leitner89@gmail.com>
2021-10-07 12:43:36 +0200

    added path helper function

--- nn_gridsearch.py
+++ nn_gridsearch.py
@@ -1,5 +1,10 @@
+import os
 from tensorflow.keras import models, layers, Model
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
+
+
+root_logdir = os.path.join(os.curdir, 'custom_logs')
 
 
 def make_model(
@@ -8,9 +13,7 @@ def make_model(
     activation: str = 'relu', 
     neurons: int = 32,
     loss: str = 'binary_crossentropy',
-    optimizer: str = 'adam',
-    learning_rate: float = .003
-) -> Model:
+    learning_rate: float = .003) -> Model:
     """
     """
 
@@ -26,12 +29,27 @@ def make_model(
     
     model.add(layers.Dense(1, activation='sigmoid'))
     
+    optimizer = Adam(lr=learning_rate)
+    
     model.compile(loss=loss, optimizer=optimizer)
     
     return model
 
 
+def logdir(hyperparam_note=None) -> str:
+    
+    
+    run_d = time.strftime(
+        f'run_%Y_%m_%d-%H_%M_%S{"_" + hyperparam_note if hyperparam_note is not None else ""}')
+    
+    directory = os.path.join(root_logdir, run_d)
+    
+    return directory
+    
+
 if __name__ == '__main__':
     test_model = make_model()
+    print(logdir('changed_batch_size_32'))
+
     
     test_model.summary()
